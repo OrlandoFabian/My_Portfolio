634cb160b840dc21c0d9ceea3a09724c47a6ed4e
OrlandoFabian <orlando.marrero6@upr.edu>
2021-02-26 18:51:06 +0000

    Created the files for the different areas and populated my home page and work experience

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['Todo bien, todo correcto y yo que me alegro'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
